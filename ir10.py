511d7a5bac914559177a51cf820ec6580046fb5a
J. Scott Berg <jsberg@bnl.gov>
2022-09-02 14:50:30 -0400

    Update IR10 geometry to place 591 MHz cavity between triplet and switching dipole

--- ir10.py
+++ ir10.py
@@ -18,26 +18,30 @@ lbeld0q1=lpld0q1-ld0fla-lflq1x-lbq1
 thd0=asin(sin(alpha) + lcendx/rhodx - lcend0/rhod0)
 thdx=asin(sin(alpha) + lcendx/rhodx)
 # 
-lq1dw_10 = 0.927704
 lwd = 5
+lcav = 5.211
+ltrpcav = 0.370
+lwc = 2
+lw10 = ltrpcav+lcav+lwc+lflq1x+lbq1-lpld0q1
+z0 = lcenxdx+lcendx+lcendxd0+lcend0
+x0 = lcendx*tan(0.5*thdx) + lcendxd0*tan(thdx) + lcend0*tan(0.5*(thdx+thd0))
 
-def fthetac(thc):
-    return lwd*(sin(thc-0.5*thd0)+sin(0.5*thc)+sin(0.5*(thc-thd0))) - (2*z0-zc)*sin(thc) + 2*x0*cos(thc)
+def fth(thl):
+    return lw10*sin(thl-thd0) + lwd*sin(0.5*(thl-thd0))/cos(0.25*thl) - z0*sin(thl) + x0*cos(thl)
 
-def dthetac(thc):
-    return lwd*(cos(thc-0.5*thd0)+0.5*(cos(0.5*thc)+cos(0.5*(thc-thd0)))) - (2*z0-zc)*cos(thc) - 2*x0*sin(thc)
+def dth(thl):
+    return lw10*cos(thl-thd0) + 0.5*lwd*cos(0.5*(thl-thd0))/cos(0.25*thl) \
+        + 0.25*lwd*sin(0.5*(thl-thd0))*tan(0.25*thl)/cos(0.25*thl) - z0*cos(thl) - x0*sin(thl)
 
-z0 = lcenxdx+lcendx+lcendxd0+lcend0+(ld0fla+lbeld0q1+lflq1x-lq1dw_10)*cos(thd0)
-x0 = lcendx*tan(0.5*thdx) + lcendxd0*tan(thdx) + lcend0*tan(0.5*(thdx+thd0))+(ld0fla+lbeld0q1+lflq1x-lq1dw_10)*sin(thd0)
-zc = 5.211+1+1.5
-
-thc = 0.0
-e1 = fthetac(thc)
+thl = thd0
+e1 = fth(thl)
+print(f"{e1:+24.17e}")
 decline = False
 while not decline or abs(e1) < abs(e0):
-    thc -= e1/dthetac(thc)
+    thl -= fth(thl)/dth(thl)
     e0 = e1
-    e1 = fthetac(thc)
+    e1 = fth(thl)
+    print(f"{e1:+24.17e}")
     if abs(e1) < abs(e0):
         decline = True
-print(f"{thc:+24.17e}")
+print(f"thl10 = {thl:+24.17e}")
