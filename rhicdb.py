733b612f2c3a189f06eedf7b9cab909bd98deab6
J. Scott Berg <jsberg@bnl.gov>
2023-02-24 19:40:07 -0500

    IR6 counter-clockwise: replace the second Q2 with a CQ8.

--- rhicdb.py
+++ rhicdb.py
@@ -641,7 +641,7 @@ ir6_slots = [slot('y',s,db)
              for s in ('yo5_trp2','yi6_trp2','yo5_cqt4','yo5_cqt5','yo5_d5','yo5_cqt6','yo5_cq7',
                        'yo5_int8_1','yo5_d8','yo5_int8_2','yo5_cq8','yo5_cq9','yo5_d9','yo5_int9_1','yo5_int9_5','yo5_int9_6',
                        'yi6_cqt4','yi6_cqt5','yi6_d5','yi6_cqt6','yi6_d6','yi6_cq7','yi6_cq8','yi6_d8','yi6_cqb9','yi6_d9')] + \
-            [slot('b',s,db) for s in ('bi5_cq8',)]
+            [slot('b',s,db) for s in ('bi5_cq8','bo3_cq8')]
 ir8_slots = [slot('y','yi7_int9_3',db),slot('y','yi7_d9',db),slot('y','yo8_d9',db),slot('y','yo8_int9_3',db)]
 ir10_slots = [slot('y','g9_dux',db),slot('b','g10_dux',db)]
 ir12_slots = [slot('b','g11_dux',db)]
